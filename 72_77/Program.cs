e78449b856775dea0e80a4a02e9efc870a502b10
TagiaHuy <146831650+TagiaHuy@users.noreply.github.com>
2024-07-18 17:55:21 +0700

    Add files via upload

---
+++ 72_77/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+// Establish connection
+string connectionString = "Server=HUNTUR\\SQLEXPRESS;Database=QLBH2;Integrated Security=SSPI;";
+SqlConnection connection = new SqlConnection(connectionString);
+connection.Open();
+// create query
+// string sqlQuery = "SELECT * FROM NEW";
+// SqlCommand command = new SqlCommand(sqlQuery, connection);
+// // Execute query 
+// SqlDataReader reader = command.ExecuteReader();
+// while (reader.Read())
+// {
+//     Console.WriteLine(reader["COLUMN1"]);
+//     Console.WriteLine(reader["COLUMN2"]);
+// }
+// reader.Close();
+// non query
+string updateQuery = "UPDATE NEW SET COLUMN1 = @newValue WHERE COLUMN1 = @id";
+SqlCommand updateCommand = new SqlCommand(updateQuery, connection);
+updateCommand.Parameters.AddWithValue("@newValue", "NEW");
+updateCommand.Parameters.AddWithValue("@id", "ROW11");
+updateCommand.ExecuteNonQuery(); 
+
+connection.Close();
